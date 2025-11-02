f759958e8835af0d6c3b0c42d89ba4551ef79f0a
Stark <baoquoctran.da@gmail.com>
2025-11-02 08:49:41 +0700

    Fix spi_cfg.sv and spi_tlm.sv

--- spi_tb_class_p1/tb/cfg/spi_cfg.sv
+++ spi_tb_class_p1/tb/cfg/spi_cfg.sv
@@ -12,24 +12,38 @@ class spi_cfg extends uvm_object;
 
   `uvm_object_utils(spi_cfg)
 
-  // declare virtual interface handles here
-  virtual apb_if apb_vif;
-  virtual spi_if spi_vif;
+  // Control register fields
+  rand bit        ass;
+  rand bit        ie;
+  rand bit        lsb;
+  rand bit        txneg;
+  rand bit        rxneg;
+  rand bit [6:0]  char_len;
 
-  // declare other configuration properties here
-  bit has_tx_fifo;
-  bit has_rx_fifo;
-  bit has_dma;
-  int tx_fifo_depth = 8;
-  int rx_fifo_depth = 8;
-  int data_width = 8;
-  
-  //
-  // NEW
-  //
-  function new(string name = "");
+  // Mask for data checking
+  bit [127:0] mask;
+
+  // Constraints for control register fields
+  constraint phase1_c {
+    ass   == 1;
+    ie    == 1;
+    lsb   == 1;
+    txneg == 1;
+    rxneg == 0;
+  }
+
+  function new(string name = "spi_cfg");
     super.new(name);
   endfunction
 
-  
+  // set_mask function to create a mask based on char_len
+  function void set_mask();
+    if (char_len == 0) begin
+      mask = '1; // All 1s for 128 bits
+    end else begin
+      mask = '0;
+      mask = (128'b1 << char_len) - 1;
+    end
+  endfunction
+
 endclass
--- spi_tb_class_p1/tb/tlm/spi_tlm.sv
+++ spi_tb_class_p1/tb/tlm/spi_tlm.sv
@@ -5,12 +5,19 @@
 //***************************************************************************************************************
 class spi_tlm extends uvm_sequence_item;
 
+  // Transaction data fields
+  rand bit [31:0] addr;    // APB address (32 bits)
+  rand bit [31:0] wdata;   // APB write data (32 bits)
+  rand bit        wr_rd;   // command (1 = write, 0 = read)
+  bit             do_reset;// (1 = reset, 0 = no action)
+  bit             do_wait; // (1 = wait a number of clocks specified in the driver, 0 = no action)
+
  `uvm_object_utils(spi_tlm)
  
   //
   // NEW
   //
-  function new(string name = "spi_tlm");
+  function new(string name = "spi_tlm"); 
     super.new(name);
   endfunction
   
